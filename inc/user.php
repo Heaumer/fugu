c31eab29713dcb36c23145fa2425de110e5f4aa4
Sophie Valentin <svalenti@polytech.unice.fr>
2012-11-11 12:11:45 +0100

    Suppression cookies deconnexion + MAJ Rapport

--- inc/user.php
+++ inc/user.php
@@ -41,6 +41,20 @@
 
 	function logout() {
 		$_SESSION['connected'] = false;
+		
+		// Détruit toutes les variables de session
+		$_SESSION = array();
+
+		// Si vous voulez détruire complètement la session, effacez également
+		// le cookie de session.
+		// Note : cela détruira la session et pas seulement les données de session !
+		if (ini_get("session.use_cookies")) {
+    		$params = session_get_cookie_params();
+    		setcookie(session_name(), '', time() - 42000,
+        		$params["path"], $params["domain"],
+        		$params["secure"], $params["httponly"]
+    		);
+		}
 		session_destroy ();
 	}
 
